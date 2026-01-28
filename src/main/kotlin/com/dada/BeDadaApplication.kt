2fc64c7cb15a1aa6a801f27ad78f59a737042119
seung-heee <tmdgmldi2595@naver.com>
2026-01-28 10:15:32 +0900

    project settings

---
+++ src/main/kotlin/com/dada/BeDadaApplication.kt
@@ -0,0 +1,11 @@
+package com.dada
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeDadaApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeDadaApplication>(*args)
+}
